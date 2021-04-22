187f518442c43c7bb4e6f31e648962c0beb388db
Abhijeet Vadera <abhijit@scaletech.xyz>
2021-04-22 14:24:09 +0530

    Remove logs

--- src/Containers/Checkout/ContactData/ContactData.js
+++ src/Containers/Checkout/ContactData/ContactData.js
@@ -6,61 +6,60 @@ import Spinner from '../../../Components/UI/Spinner/Spinner'
 import Input from '../../../Components/UI/Input/Input'
 
 
-class ContactData extends Component{
-    
-    state={
-        name:'',
-        email:'',
-        address:{
-            street:'',
-            postalCode:''
+class ContactData extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
         },
-        loading:false
+        loading: false
     }
 
-    orderHandler=(event)=>{
+    orderHandler = (event) => {
         event.preventDefault();
-        this.setState({loading:true})
+        this.setState({ loading: true })
         // setTimeout(function() { alert('Order Booked ;P '); }, 10);
-        const order ={
-            ingredients:this.props.ingredients,
-            price:this.props.price,
-            customer:{
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
                 name: 'Av',
-                address:{
-                    street:'street 1',
-                    zipCode:'123456',
-                    country:'India'
+                address: {
+                    street: 'street 1',
+                    zipCode: '123456',
+                    country: 'India'
                 },
-                email:'abc@gmail.com',
+                email: 'abc@gmail.com',
             },
-            deliveryMode:'Fastest'
+            deliveryMode: 'Fastest'
         }
-        axios.post('/orders.json',order)
-            .then(response=>{
-                this.setState({loading:false});
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({ loading: false });
                 this.props.history.push('/');
             })
-            .catch(error=>{
-                this.setState({loading:false})
+            .catch(error => {
+                this.setState({ loading: false })
                 console.log(error)
             });
         // this.purchaseHandlerClosed()
-        console.log(this.props.ingredients);    
     }
-    render(){
+    render() {
         let form = (<form>
-            <Input inputtype='input' type="text" name="name" placeholder="Your name"/>
-            <Input inputtype='input' type="text" name="email" placeholder="Email"/>
-            <Input inputtype='input' type="text" name="street" placeholder="Street"/>
-            <Input inputtype='input' type="text" name="postal" placeholder="Postal Code"/>
+            <Input inputtype='input' type="text" name="name" placeholder="Your name" />
+            <Input inputtype='input' type="text" name="email" placeholder="Email" />
+            <Input inputtype='input' type="text" name="street" placeholder="Street" />
+            <Input inputtype='input' type="text" name="postal" placeholder="Postal Code" />
 
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>);
-        if(this.state.loading){
-            form=<Spinner />
+        if (this.state.loading) {
+            form = <Spinner />
         }
-        return(
+        return (
             <div className={classes.ContactData}>
                 <h3>Enter Yout Contact Data..!</h3>
                 {form}
